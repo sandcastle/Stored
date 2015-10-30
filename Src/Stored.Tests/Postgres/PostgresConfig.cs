fefc0a80e1e4db19a25e28eddd33e073b6ee5c50
Glenn Morton <glenn@sandcastle.io>
2015-10-31 00:37:13 +0800

    Added password to postgres connection string (if available)

--- Src/Stored.Tests/Postgres/PostgresConfig.cs
+++ Src/Stored.Tests/Postgres/PostgresConfig.cs
@@ -15,11 +15,12 @@ namespace Stored.Tests.Postgres
                 password = "Password=" + password + ";";
             }
 
-            ConnectionString = String.Format("Server={0};Port={1};Database={2};User Id={3};{4}", 
+            ConnectionString = String.Format("Server={0};Port={1};Database={2};User Id={3};{4}",
                 GetVariable("POSTGRES_HOST", "192.168.99.100"),
                 GetVariable("POSTGRES_PORT", "5432"),
                 GetVariable("POSTGRES_DB", "stored_db"),
-                GetVariable("POSTGRES_USER", "postgres"));
+                GetVariable("POSTGRES_USER", "postgres"),
+                password);
 
             Console.WriteLine(ConnectionString);
         }
